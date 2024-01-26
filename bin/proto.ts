3f7fafd164f8263a94235cfa1add8451b83cbdc6
Nicolas Delorme <nic@4ks.io>
2024-01-26 09:20:14 +0000

    using node 20 and convert bin/proto to ts

--- bin/proto.ts
+++ bin/proto.ts
@@ -1,13 +1,13 @@
-const { join } = require('path');
-const { execSync } = require('child_process');
-const { rimrafSync } = require('rimraf');
-const { protoPath: healthProtoPath } = require('grpc-health-check');
-const { copyFileSync } = require('fs');
+import { join } from 'path';
+import { execSync } from 'child_process';
+import { rimrafSync } from 'rimraf';
+import { protoPath as healthProtoPath } from 'grpc-health-check';
+import { copyFileSync } from 'fs';
 
 const PROTO_DIR = join(__dirname, '../proto');
 const MODEL_DIR = join(__dirname, '../src/models');
-const PROTOC_PATH = join(__dirname, "../node_modules/grpc-tools/bin/protoc");
-const PLUGIN_PATH = join(__dirname, "../node_modules/.bin/protoc-gen-ts_proto");
+const PROTOC_PATH = join(__dirname, '../node_modules/grpc-tools/bin/protoc');
+const PLUGIN_PATH = join(__dirname, '../node_modules/.bin/protoc-gen-ts_proto');
 
 rimrafSync(`${MODEL_DIR}/*`, {
   glob: { ignore: `${MODEL_DIR}/tsconfig.json` },
@@ -16,13 +16,7 @@ rimrafSync(`${MODEL_DIR}/*`, {
 copyFileSync(healthProtoPath, join(PROTO_DIR, 'health.proto'));
 
 // https://github.com/stephenh/ts-proto/blob/main/README.markdown#supported-options
-const tsProtoOpt = [
-  'outputServices=grpc-js',
-  'env=node',
-  'useOptionals=messages',
-  'exportCommonSymbols=false',
-  'esModuleInterop=true',
-];
+const tsProtoOpt = ['outputServices=grpc-js', 'env=node', 'useOptionals=messages', 'exportCommonSymbols=false', 'esModuleInterop=true'];
 
 const protoConfig = [
   `--plugin=${PLUGIN_PATH}`,
@@ -32,5 +26,5 @@ const protoConfig = [
 ];
 
 // https://github.com/stephenh/ts-proto#usage
-execSync(`${PROTOC_PATH} ${protoConfig.join(" ")}`);
+execSync(`${PROTOC_PATH} ${protoConfig.join(' ')}`);
 console.log(`> Proto models created: ${MODEL_DIR}`);
